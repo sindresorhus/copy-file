b2afeb0b1b73df1f94c04ff9345b13863ce9cb18
Michael Mayer <michael@schnittstabil.de>
2015-03-15 12:00:23 +0100

    reduce complexity

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 var path = require('path');
 var fs = require('graceful-fs');
 var mkdirp = require('mkdirp');
-var assign = require('object-assign');
+var objectAssign = require('object-assign');
 var onetime = require('onetime');
 var VError = require('verror');
 
 module.exports = function (src, dest, opts, cb) {
 	if (!src || !dest) {
-		throw new assign(new VError('`src` and `dest` required'), { name: 'CpFileError' })
+		var err = new VError('`src` and `dest` required');
+		err.name = 'CpFileError';
+		throw err;
 	}
 
 	if (typeof opts !== 'object') {
@@ -17,14 +19,15 @@ module.exports = function (src, dest, opts, cb) {
 	}
 
 	cb = onetime(cb || function () {});
-	opts = assign({overwrite: true}, opts);
+	opts = objectAssign({overwrite: true}, opts);
 
 	var read = fs.createReadStream(src);
 	var readListener = onetime(startWrite);
 
 	read.on('error', function (err) {
 		if (err) {
-			err = assign(new VError(err, 'cannot read from \'%s\'', src), { name: 'CpFileError' });
+			err = new VError(err, 'cannot read from `' + src + '`');
+			err.name = 'CpFileError';
 		}
 
 		cb(err);
@@ -35,7 +38,9 @@ module.exports = function (src, dest, opts, cb) {
 	function startWrite() {
 		mkdirp(path.dirname(dest), function (err) {
 			if (err && err.code !== 'EEXIST') {
-				cb(assign(new VError(err, 'cannot create directory \'%s\'', dest), { name: 'CpFileError' }));
+				err = new VError(err, 'cannot create directory `' + path.dirname(dest) + '`');
+				err.name = 'CpFileError';
+				cb(err);
 				return;
 			}
 
@@ -46,20 +51,24 @@ module.exports = function (src, dest, opts, cb) {
 					cb();
 					return;
 				}
-
-				cb(assign(new VError(err, 'cannot write to \'%s\'', dest), { name: 'CpFileError' }));
+				err = new VError(err, 'cannot write to `' + dest + '`');
+				err.name = 'CpFileError';
+				cb(err);
 			});
 
 			write.on('close', function () {
 				fs.lstat(src, function (err, stats) {
 					if (err) {
-						cb(assign(new VError(err, 'lstat \'%s\' failed', src), { name: 'CpFileError' }));
+						err = new VError(err, 'lstat `' + src + '` failed');
+						err.name = 'CpFileError';
+						cb(err);
 						return;
 					}
 
 					fs.utimes(dest, stats.atime, stats.mtime, function (err) {
 						if (err) {
-							err = assign(new VError(err, 'utimes \'%s\' failed', dest), { name: 'CpFileError' });
+							err = new VError(err, 'utimes `' + dest + '` failed');
+							err.name = 'CpFileError';
 						}
 						cb(err);
 					});
@@ -74,10 +83,12 @@ module.exports = function (src, dest, opts, cb) {
 
 module.exports.sync = function (src, dest, opts) {
 	if (!src || !dest) {
-		throw new assign(new VError('`src` and `dest` required'), { name: 'CpFileError' })
+		var err = new VError('`src` and `dest` required');
+		err.name = 'CpFileError';
+		throw err;
 	}
 
-	opts = assign({overwrite: true}, opts);
+	opts = objectAssign({overwrite: true}, opts);
 
 	var read = fs.openSync(src, 'r');
 	var BUF_LENGTH = 100 * 1024;
@@ -90,7 +101,9 @@ module.exports.sync = function (src, dest, opts) {
 		try {
 			return fs.readSync(read, buf, 0, BUF_LENGTH, pos);
 		} catch (err) {
-			throw assign(new VError(err, 'cannot read from \'%s\'', src), { name: 'CpFileError' });
+			err = new VError(err, 'cannot read from `' + src + '`');
+			err.name = 'CpFileError';
+			throw err;
 		}
 	}
 
@@ -98,7 +111,9 @@ module.exports.sync = function (src, dest, opts) {
 		try {
 			fs.writeSync(write, buf, 0, bytesRead);
 		} catch (err) {
-			throw assign(new VError(err, 'cannot write to \'%s\'', dest), { name: 'CpFileError' });
+			err = new VError(err, 'cannot write to `' + dest + '`');
+			err.name = 'CpFileError';
+			throw err;
 		}
 	}
 
@@ -106,7 +121,9 @@ module.exports.sync = function (src, dest, opts) {
 		mkdirp.sync(path.dirname(dest));
 	} catch (err) {
 		if (err.code !== 'EEXIST') {
-			throw assign(new VError(err, 'cannot create directory \'%s\'', dest), { name: 'CpFileError' });
+			err = new VError(err, 'cannot create directory `' + path.dirname(dest) + '`');
+			err.name = 'CpFileError';
+			throw err;
 		}
 	}
 
@@ -116,7 +133,9 @@ module.exports.sync = function (src, dest, opts) {
 		if (!opts.overwrite && err.code === 'EEXIST') {
 			return;
 		}
-		throw assign(new VError(err, 'cannot write to \'%s\'', dest), { name: 'CpFileError' });
+		err = new VError(err, 'cannot write to `' + dest + '`');
+		err.name = 'CpFileError';
+		throw err;
 	}
 
 	writeSync();
