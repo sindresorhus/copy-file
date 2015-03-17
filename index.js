e4174486d8b0292667e7c5767cbdb4d7d6545b30
Michael Mayer <michael@schnittstabil.de>
2015-03-17 21:46:13 +0100

    s/VError/nested-error-stacks/

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ var fs = require('graceful-fs');
 var mkdirp = require('mkdirp');
 var objectAssign = require('object-assign');
 var onetime = require('onetime');
-var VError = require('verror');
+var NestedError = require('nested-error-stacks');
+var util = require('util');
+
+function CpFileError(message, nested) {
+	NestedError.call(this, message, nested);
+	objectAssign(this, nested, {message: message});
+}
+
+util.inherits(CpFileError, NestedError);
+
+CpFileError.prototype.name = 'CpFileError';
 
 module.exports = function (src, dest, opts, cb) {
 	if (!src || !dest) {
-		var err = new VError('`src` and `dest` required');
-		err.name = 'CpFileError';
-		throw err;
+		throw new CpFileError('`src` and `dest` required');
 	}
 
 	if (typeof opts !== 'object') {
@@ -26,8 +34,7 @@ module.exports = function (src, dest, opts, cb) {
 
 	read.on('error', function (err) {
 		if (err) {
-			err = new VError(err, 'cannot read from `' + src + '`');
-			err.name = 'CpFileError';
+			err = new CpFileError('cannot read from `' + src + '`: ' + err.message, err);
 		}
 
 		cb(err);
@@ -38,9 +45,7 @@ module.exports = function (src, dest, opts, cb) {
 	function startWrite() {
 		mkdirp(path.dirname(dest), function (err) {
 			if (err && err.code !== 'EEXIST') {
-				err = new VError(err, 'cannot create directory `' + path.dirname(dest) + '`');
-				err.name = 'CpFileError';
-				cb(err);
+				cb(new CpFileError('cannot create directory `' + path.dirname(dest) + '`: ' + err.message, err));
 				return;
 			}
 
@@ -51,24 +56,19 @@ module.exports = function (src, dest, opts, cb) {
 					cb();
 					return;
 				}
-				err = new VError(err, 'cannot write to `' + dest + '`');
-				err.name = 'CpFileError';
-				cb(err);
+				cb(new CpFileError(err, 'cannot write to `' + dest + '`: ' + err.message, err));
 			});
 
 			write.on('close', function () {
 				fs.lstat(src, function (err, stats) {
 					if (err) {
-						err = new VError(err, 'lstat `' + src + '` failed');
-						err.name = 'CpFileError';
-						cb(err);
+						cb(new CpFileError('lstat `' + src + '` failed: ' + err.message, err));
 						return;
 					}
 
 					fs.utimes(dest, stats.atime, stats.mtime, function (err) {
 						if (err) {
-							err = new VError(err, 'utimes `' + dest + '` failed');
-							err.name = 'CpFileError';
+							err = new CpFileError('utimes `' + dest + '` failed: ' + err.message, err);
 						}
 						cb(err);
 					});
@@ -80,12 +80,9 @@ module.exports = function (src, dest, opts, cb) {
 	}
 };
 
-
 module.exports.sync = function (src, dest, opts) {
 	if (!src || !dest) {
-		var err = new VError('`src` and `dest` required');
-		err.name = 'CpFileError';
-		throw err;
+		throw new CpFileError('`src` and `dest` required');
 	}
 
 	opts = objectAssign({overwrite: true}, opts);
@@ -101,9 +98,7 @@ module.exports.sync = function (src, dest, opts) {
 		try {
 			return fs.readSync(read, buf, 0, BUF_LENGTH, pos);
 		} catch (err) {
-			err = new VError(err, 'cannot read from `' + src + '`');
-			err.name = 'CpFileError';
-			throw err;
+			throw new CpFileError('cannot read from `' + src + '`: ' + err.message, err);
 		}
 	}
 
@@ -111,9 +106,7 @@ module.exports.sync = function (src, dest, opts) {
 		try {
 			fs.writeSync(write, buf, 0, bytesRead);
 		} catch (err) {
-			err = new VError(err, 'cannot write to `' + dest + '`');
-			err.name = 'CpFileError';
-			throw err;
+			throw new CpFileError('cannot write to `' + dest + '`: ' + err.message, err);
 		}
 	}
 
@@ -121,9 +114,7 @@ module.exports.sync = function (src, dest, opts) {
 		mkdirp.sync(path.dirname(dest));
 	} catch (err) {
 		if (err.code !== 'EEXIST') {
-			err = new VError(err, 'cannot create directory `' + path.dirname(dest) + '`');
-			err.name = 'CpFileError';
-			throw err;
+			throw new CpFileError('cannot create directory `' + path.dirname(dest) + '`: ' + err.message, err);
 		}
 	}
 
@@ -133,9 +124,7 @@ module.exports.sync = function (src, dest, opts) {
 		if (!opts.overwrite && err.code === 'EEXIST') {
 			return;
 		}
-		err = new VError(err, 'cannot write to `' + dest + '`');
-		err.name = 'CpFileError';
-		throw err;
+		throw new CpFileError('cannot write to `' + dest + '`: ' + err.message, err);
 	}
 
 	writeSync();
